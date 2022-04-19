c65e3fe0181dd0969e098210d858377fa037bfb3
Abbas Naqvi <75449193+AbbasNaqvi10@users.noreply.github.com>
2022-04-19 11:36:43 +0500

    progress-bar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Home from './pages/home/home'
+import Form from './pages/form/form'
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-     <Home />
+     <Form />
     </div>
   );
 }
